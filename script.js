25b9844afbd7a5b7021c4567e0920a5a387369b5
Jason Rossi <jrossi3@stevens.edu>
2022-04-07 03:28:35 -0400

    Updated projects.html
    Added button features to be hidden and displayed on command, structured projects.html page

--- script.js
+++ script.js
@@ -5,36 +5,36 @@ let counterMenu = true;
 
 
 // Show active menu when scrolling
-const highlightMenu = () => {
-  const elem = document.querySelector('.highlight');
-  const homeMenu = document.querySelector('#home-page');
-  const aboutMenu = document.querySelector('#resume-page');
-  const servicesMenu = document.querySelector('#projects-page');
-  let scrollPos = window.scrollY;
+// const highlightMenu = () => {
+//   const elem = document.querySelector('.highlight');
+//   const homeMenu = document.querySelector('#home-page');
+//   const aboutMenu = document.querySelector('#resume-page');
+//   const servicesMenu = document.querySelector('#projects-page');
+//   let scrollPos = window.scrollY;
 
-  // adds 'highlight' class to my menu items
-  if (window.innerWidth > 960 && scrollPos < 500) {
-    homeMenu.classList.add('highlight');
-    aboutMenu.classList.remove('highlight');
-    return;
-  } else if (window.innerWidth > 960 && scrollPos < 1200) {
-    aboutMenu.classList.add('highlight');
-    homeMenu.classList.remove('highlight');
-    servicesMenu.classList.remove('highlight');
-    return;
-  } else if (window.innerWidth > 960 && scrollPos < 2345) {
-    servicesMenu.classList.add('highlight');
-    aboutMenu.classList.remove('highlight');
-    return;
-  }
+// adds 'highlight' class to my menu items
+//   if (window.innerWidth > 960 && scrollPos < 500) {
+//     homeMenu.classList.add('highlight');
+//     aboutMenu.classList.remove('highlight');
+//     return;
+//   } else if (window.innerWidth > 960 && scrollPos < 1200) {
+//     aboutMenu.classList.add('highlight');
+//     homeMenu.classList.remove('highlight');
+//     servicesMenu.classList.remove('highlight');
+//     return;
+//   } else if (window.innerWidth > 960 && scrollPos < 2345) {
+//     servicesMenu.classList.add('highlight');
+//     aboutMenu.classList.remove('highlight');
+//     return;
+//   }
 
-  if ((elem && window.innerWIdth < 960 && scrollPos < 600) || elem) {
-    elem.classList.remove('highlight');
-  }
-};
+//   if ((elem && window.innerWIdth < 960 && scrollPos < 600) || elem) {
+//     elem.classList.remove('highlight');
+//   }
+// };
 
-window.addEventListener('scroll', highlightMenu);
-window.addEventListener('click', highlightMenu);
+// window.addEventListener('scroll', highlightMenu);
+// window.addEventListener('click', highlightMenu);
 
 const mobileMenu = () => {
   menu.classList.toggle('is-active');
@@ -50,6 +50,6 @@ function helper() {
   }
 }
 
-AOS.init({
-  easing: 'ease-in-quad',
-});
+// AOS.init({
+//   easing: 'ease-in-quad',
+// });
--- videosOfProjects.js
+++ videosOfProjects.js
@@ -1,9 +1,14 @@
 var myVideo = document.getElementById("video100"); 
+let spring = document.getElementById("video100");
+let buttonSpring= document.getElementById('springBreak')
+let playPauseButton = document.getElementById('playPause')
 function playPause() { 
-    if (myVideo.paused) 
-    myVideo.play(); 
-    else 
-    myVideo.pause(); 
+    if (myVideo.paused) {
+        myVideo.play(); 
+    }
+    else {
+        myVideo.pause(); 
+    }
 } 
 function makeBig() { 
     myVideo.width = 1000; 
@@ -13,4 +18,16 @@ function makeSmall() {
 } 
 function makeNormal() { 
     myVideo.width = 420; 
-} 
+}
+
+function displaySpringBreak(){
+    if (spring.hidden == false){
+        spring.hidden = true
+        playPauseButton.hidden = true
+        buttonSpring.innerHTML = 'Display Project'
+    }else {
+        spring.hidden = false
+        playPauseButton.hidden = false
+        buttonSpring.innerHTML = 'Hide Project'
+    }
+}
