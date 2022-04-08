3825e367e7b6cc4a8b41bbd2232b19633d9a2071
Jason Rossi <jrossi3@stevens.edu>
2022-04-08 02:45:15 -0400

    Added Quiz App
    Added quiz app video, updated to be consistent with other projects

--- videosOfProjects.js
+++ videosOfProjects.js
@@ -1,31 +1,53 @@
 var myVideo = document.getElementById("video100"); 
-let spring = document.getElementById("video100");
-let buttonSpring= document.getElementById('springBreak')
+let spring = document.getElementById("videoSpring");
+let buttonSpring = document.getElementById('springBreak')
 let playPauseButton = document.getElementById('playPause')
+let playPauseButtonQuiz = document.getElementById('playPauseQuiz')
+let quiz = document.getElementById('videoQuiz')
+let quizButton = document.getElementById('quizApp')
 function playPause() { 
-    if (myVideo.paused) {
-        myVideo.play(); 
+    if (spring.paused) {
+        spring.play(); 
     }
     else {
-        myVideo.pause(); 
+        spring.pause(); 
     }
-} 
-function makeBig() { 
-    myVideo.width = 1000; 
-} 
-function makeSmall() { 
-    myVideo.width = 320; 
-} 
-function makeNormal() { 
-    myVideo.width = 420; 
 }
 
+function playPauseQuiz() { 
+    if (quiz.paused) {
+        quiz.play(); 
+    }
+    else {
+        quiz.pause(); 
+    }
+} 
+// function makeBig() { 
+//     myVideo.width = 1000; 
+// } 
+// function makeSmall() { 
+//     myVideo.width = 320; 
+// } 
+// function makeNormal() { 
+//     myVideo.width = 420; 
+// }
+function displayQuizApp() {
+    if (quiz.hidden == false){
+        quiz.hidden = true
+        playPauseButtonQuiz.hidden = true
+        quizButton.innerHTML = 'Display Project'
+    } else {
+        quiz.hidden = false
+        playPauseButtonQuiz.hidden = false
+        quizButton.innerHTML = 'Hide Project'
+    }
+}
 function displaySpringBreak(){
     if (spring.hidden == false){
         spring.hidden = true
         playPauseButton.hidden = true
         buttonSpring.innerHTML = 'Display Project'
-    }else {
+    } else {
         spring.hidden = false
         playPauseButton.hidden = false
         buttonSpring.innerHTML = 'Hide Project'
