984906295923191d4a38e141dd52fe0dc10f9460
Jason Rossi <jrossi3@stevens.edu>
2022-07-27 18:09:01 -0400

    Added the Timer video along with the button features

--- videosOfProjects.js
+++ videosOfProjects.js
@@ -5,6 +5,9 @@ let playPauseButton = document.getElementById('playPause')
 let playPauseButtonQuiz = document.getElementById('playPauseQuiz')
 let quiz = document.getElementById('videoQuiz')
 let quizButton = document.getElementById('quizApp')
+let timer = document.getElementById('videoTimer')
+let timerButton = document.getElementById('timerButton')
+let playPauseButtonTimer = document.getElementById('playPauseTimer')
 function playPause() { 
     if (spring.paused) {
         spring.play(); 
@@ -20,6 +23,15 @@ function playPauseQuiz() {
     }
     else {
         quiz.pause(); 
+    }
+} 
+
+function playPauseTimer() { 
+    if (timer.paused) {
+        timer.play(); 
+    }
+    else {
+        timer.pause(); 
     }
 } 
 // function makeBig() { 
@@ -51,5 +63,16 @@ function displaySpringBreak(){
         spring.hidden = false
         playPauseButton.hidden = false
         buttonSpring.innerHTML = 'Hide Project'
+    }
+}
+function displayTimer(){
+    if (timer.hidden == false){
+        timer.hidden = true
+        playPauseButtonTimer.hidden = true
+        timerButton.innerHTML = 'Display Project'
+    } else {
+        timer.hidden = false
+        playPauseButtonTimer.hidden = false
+        timerButton.innerHTML = 'Hide Project'
     }
 }
